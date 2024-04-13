299aafadbf302f47d5ffce8109c37ce1e1aa8b00
Noureddine-Elkhalifa <mnour.elkhalifa@gmail.com>
2024-04-13 17:02:52 +0000

    Home responsiveness

--- src/Components/layout/home.js
+++ src/Components/layout/home.js
@@ -15,10 +15,11 @@ export default function Acceuil()
             <div className="Part1Container1">
                 <h1>Retrouvez, vérifier et déclarez en toute simplicité</h1>
                 <p>Avec notre service, trouvez vos objets égarés, vérifiez le statut d'un téléphone avant l'achat, et signalez les trouvailles ou les pertes. Facile et rapide!</p>
-                
-                <button>Telephone</button>
-                <button>Objets perdus</button>
-                <button>Objets trouvés</button>
+                <div className="homeButtons">           
+                    <button>Telephone</button>
+                    <button>Objets perdus</button>
+                    <button>Objets trouvés</button>
+                </div>
                 
             </div>
             <div className="Part2Container1" >
@@ -26,22 +27,23 @@ export default function Acceuil()
             </div>
 
     </div>
-    <div className="Container2Acceuil">
     <h1>Nos Services</h1>
+    <div className="Container2Acceuil">
+    
     <div className="mycontainer">
         <div className="mycard" style={{"backgroundColor":"white"}}>
             <h5>Déclarer votre téléphone</h5>
             <p>Avec un statut volé, perdu ou possédé</p>
         </div>
         <div className="myflech">
-            <FontAwesomeIcon icon={faArrowRight} style={{color:"white",marginLeft:"45px"}}/>
+            <FontAwesomeIcon icon={faArrowRight} style={{color:"white"}}/>
         </div>
         <div className="mycard" style={{"backgroundColor":"white"}}>
             <h5>Quelqu'un a cherché votre IMEI</h5>
             <p>Il va le trouver dans notre système</p>
         </div>
         <div className="myflech">
-            <FontAwesomeIcon icon={faArrowRight} style={{color:"white",marginLeft:"45px"}}/>
+            <FontAwesomeIcon icon={faArrowRight} style={{color:"white"}}/>
         </div>
         <div className="mycard" style={{"backgroundColor":"white"}}>
             <h5>Notification par E-mail</h5>
@@ -54,14 +56,14 @@ export default function Acceuil()
             <p>Vous pouvez ajouter vos objets perdus ou trouvés avec facilité</p>
         </div>
         <div className="myflech">
-            <FontAwesomeIcon icon={faArrowRight} style={{color:"white",marginLeft:"45px"}}/>
+            <FontAwesomeIcon icon={faArrowRight} style={{color:"white"}}/>
         </div>
         <div className="mycard" style={{"backgroundColor":"white"}}>
             <h5>Le propriétaire a retrouvé son objet/Quelqu'un a trouvé le propriétaire</h5>
             <p>Vous pouvez être en contact</p>
         </div>
         <div className="myflech">
-            <FontAwesomeIcon icon={faArrowRight} style={{color:"white",marginLeft:"45px"}}/>
+            <FontAwesomeIcon icon={faArrowRight} style={{color:"white"}}/>
         </div>
         <div className="mycard" style={{"backgroundColor":"white"}}>
             <h5>Récupérer vos objets</h5>
